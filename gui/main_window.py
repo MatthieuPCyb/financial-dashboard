91733696f14c7a0dfe7920ac71ce1f29399dd3f9
Matthieu <matthieu.pohin@gmail.com>
2026-08-01 08:37:55 +0200

    Ajout des scripts pour l'interface

---
+++ gui/main_window.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import *
+
+
+class MainApp():
+    
+    def __init__(self, root: Tk):
+        self.root = root
+        self.root.title("Tableau de bord")
+        
